c237a3b6e53f42ebeaf411f161ddaef0531603e0
neyustudies <science-cat@gmx.net>
2020-05-25 13:54:16 +0200

    gelöschte Methoden aus .hpp entfernt

--- source/quicksort.hpp
+++ source/quicksort.hpp
@@ -3,14 +3,13 @@
 #include <vector>
 #include <algorithm>
 
+
 class Quicksort {
   
   public:
 
     int   partition   (std::vector<int>& A, int p, int r) const;
     void  quicksort   (std::vector<int>& A, int p, int r) const;
-    void  sort        (std::vector<int>& A) const;
-    void  print       (std::vector<int>& A) const;
 
 };
 
