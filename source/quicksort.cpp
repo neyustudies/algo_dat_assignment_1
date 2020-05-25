f33971d22bd4830b9a29bdedf499ef0a03213a8b
neyustudies <science-cat@gmx.net>
2020-05-25 13:06:24 +0200

    endlich richtig angepasst, sieht genauso aus wie der Pseudocode :)

--- source/quicksort.cpp
+++ source/quicksort.cpp
@@ -1,37 +1,24 @@
 #include "quicksort.hpp"
 #include <iostream>
 
-
 void Quicksort::quicksort(std::vector<int>& A, int p, int r) const{
-  if(p >= r)
-    return;
-  int q = partition(A, p, r);
-  quicksort(A, p, q - 1);
-  quicksort(A, q + 1, r);
+  if(p < r) {
+      int q = partition(A, p, r);
+      quicksort(A, p, q - 1);
+      quicksort(A, q + 1, r);
+  }
 }
 
 int Quicksort::partition(std::vector<int>& A, int p, int r) const {
-  int x = (p + r) / 2;
-  std::swap(A[x], A[r]);
-  int i = p;
+  auto x = A[r];
+  int i = p - 1;
   for(int j = p; j < r; ++j) {
-    if(A[j] < A[r]) {
+    if(A[j] <= x) {
       std::swap(A[i], A[j]);
       ++i;
-    }
-  } std::swap(A[i], A[r]);
-    return i;
-}
-
-void Quicksort::sort(std::vector<int>& A) const {
-  int s = A.size();
-  quicksort(A, 0, s - 1);
-}
-
-void Quicksort::print(std::vector<int>& A) const {
-  for(int i : A) {
-    std::cout << i << " ";
-  } std::cout << "\n";
+    } std::swap(A[i], A[r]);
+      return i;
+  }
 }
 
 
